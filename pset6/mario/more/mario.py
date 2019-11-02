768e3847129ac43d95a679339cc66c54d69e64b3
AnnaKudryashova <annacogito@gmail.com>
2019-11-02 14:02:52 -0400

    Update mario.py

--- pset6/mario/more/mario.py
+++ pset6/mario/more/mario.py
@@ -1,1 +1,16 @@
+from cs50 import get_int
 
+def main():
+    s=0
+
+    while s==0:
+        h=get_int("Enter height: ")
+        if h >0 and h<9:
+            height=range(h)
+            for i in height:
+                s="#"*(i+1)
+                print(' '*(h-i)+s+'  '+s)
+        else:
+            print('Your number should be an integer between 1 and 8')
+
+main()
